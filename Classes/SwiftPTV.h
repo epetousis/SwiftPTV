cdc0141b9a45e866dc083290096380786ee751ae
evilgoldfish <evilgoldfish@users.noreply.github.com>
2017-11-05 23:12:49 +1100

    Fixed import of iOS only header

--- Classes/SwiftPTV.h
+++ Classes/SwiftPTV.h
@@ -5,7 +5,7 @@
 //  Created by evilgoldfish on 5/11/17.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 FOUNDATION_EXPORT double SwiftPTViOSVersionNumber;
 FOUNDATION_EXPORT const unsigned char SwiftPTViOSVersionString[];
