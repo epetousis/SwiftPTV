83a36dc12aa67ab6d19306013b5440c6134b6ba9
evilgoldfish <evilgoldfish@users.noreply.github.com>
2017-11-05 21:40:50 +1100

    Stopped using CocoaPods template

---
+++ Classes/SwiftPTV.h
@@ -0,0 +1,14 @@
+//
+//  SwiftPTV.h
+//  SwiftPTV
+//
+//  Created by evilgoldfish on 5/11/17.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double SwiftPTViOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char SwiftPTViOSVersionString[];
+
+#import "SwiftPTV-Swift.h"
+
--- Example/Pods/Target Support Files/SwiftPTV/SwiftPTV-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftPTV : NSObject
-@end
-@implementation PodsDummy_SwiftPTV
-@end
