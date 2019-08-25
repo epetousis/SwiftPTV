5eae27a17e7976ca4d1e04b15e8c38e32f2e5e3a
Evan Petousis <evan@petousis.net>
2019-08-25 13:30:57 +1000

    Added Swift platforms

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftPTV",
+    platforms: [
+       .macOS(.v10_13), .iOS(.v11),
+    ],
     products: [
         .library(name: "SwiftPTV", targets: ["SwiftPTV"])
     ],
